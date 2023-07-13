3f8fd518ba42fc96709e13e97cdd26a6c47a43d4
Priscilla <prisciillamoraiis@gmail.com>
2023-07-13 19:34:02 -0300

    início do buger Eats

---
+++ cypress/app/register.cy.ts
@@ -0,0 +1,32 @@
+describe(" Suite de registro do bugerEats",() => {
+
+    it("Deve ser possivel cadastrar com sucesso", () =>{
+
+    });
+
+    it("Deve ser possivel retornar para a Home", () =>{
+
+    });
+
+    it("Deve mostrar mensagem de erro quando campo nome estiver vazio", () =>{
+
+    });
+
+    it("Deve mostrar mensagem de erro com CPF inválido", () =>{
+
+    });
+
+    it("Deve mostrar mensagem de erro quando email estiver fora do padrão", () =>{
+
+    });
+
+    it("Deve mostrar mensagem de erro quando email estiver incompleto", () =>{
+
+    });
+
+    it("Deve mostrar mensagem de erro quando campos obrigatórios estiverem vazios", () =>{
+
+    });
+
+
+});
